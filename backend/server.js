7c9f8585b0d09f5fb5ed924d207e6b808697d3b5
HodeX7 <mehtaakshar1234@gmail.com>
2023-11-20 15:52:59 +0530

    Removed comment

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 // const multer = require('multer');
-const path = require('path');
+const path = require("path");
 const PORT = process.env.PORT || 3500;
 const { logger } = require("./middleware/logger");
 const errorHandler = require("./middleware/errorHandler");
@@ -10,30 +10,17 @@ const cors = require("cors");
 const corsOptions = require("./config/corsOptions");
 const { default: mongoose } = require("mongoose");
 
-
-// const storage = multer.diskStorage({
-//   destination: (req, file, cb) => {
-//     cb(null, path.join(__dirname, "/uploads")); // Define the directory where uploaded files will be stored
-//   },
-//   filename: (req, file, cb) => {
-//     cb(null, Date.now() + path.extname(file.originalname)); // Use a timestamp as the filename
-//   },
-// });
-
-// const fileUpload = multer({ storage });
-
 app.use(logger);
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
 app.use("/public", express.static(path.join(__dirname, "/public")));
-app.use('/uploads', express.static(path.join(__dirname, "/uploads")));
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
 app.use("/", require("./routes/root"));
 app.use("/user", require("./routes/UserRoutes"));
 app.use("/dog", require("./routes/DogRoutes"));
 app.use("/dog", require("./routes/DogMiscRoutes"));
-
 
 app.all("*", (req, res) => {
   res.status(404);
@@ -49,10 +36,14 @@ app.all("*", (req, res) => {
 app.use(errorHandler);
 
 mongoose.set("strictQuery", false);
-mongoose.connect("mongodb://localhost:27017/DogNGOApp").then(() => {
-  console.log("MongoDB Connected");
-  app.listen(PORT, '192.168.1.7', () => console.log(`Server running on port ${PORT}`));
-})
-.catch((err) => {
-  console.log(err);
-});
+mongoose
+  .connect("mongodb://localhost:27017/DogNGOApp")
+  .then(() => {
+    console.log("MongoDB Connected");
+    app.listen(PORT, "192.168.1.7", () =>
+      console.log(`Server running on port ${PORT}`)
+    );
+  })
+  .catch((err) => {
+    console.log(err);
+  });
