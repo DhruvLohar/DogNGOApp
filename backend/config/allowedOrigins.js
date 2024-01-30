df4d346da046929dcbdd6d5a99f9e743114501fd
DhruvLohar <dhruvlohar09@gmail.com>
2024-01-30 20:51:35 +0530

    updated allowed origins

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -4,13 +4,12 @@ const allowedOrigins = [
     "http://localhost:8081",
     "http://j-app.in", "http://www.j-app.in", 
     "https://j-app.in", "https://www.j-app.in", 
-    "http://api.j-app.in", "https://api.j-app.in", 
+    "http://api.j-app.in", "https://api.j-app.in",
+    
+    "https://mb.j-app.in", "http://mb.j-app.in",
+    "https://kd.j-app.in", "http://kd.j-app.in",
+    "https://ic.j-app.in", "http://ic.j-app.in",
+    "https://nk.j-app.in", "http://nk.j-app.in",
     process.env.REMOTE_CLIENT_APP];
 
 module.exports = allowedOrigins;
-
-/*
-
-npm ERR! code 254npm ERR! path /home/cyilqndy/nodevenv/api.jrbmc.online/16/lib/node_modules/bcryptnpm ERR! command failednpm ERR! command sh -c -- node-pre-gyp install --fallback-to-buildnpm ERR! /home/cyilqndy/nodevenv/api.jrbmc.online/16/bin/node: fork: retry: Resource temporarily unavailablenpm ERR! /home/cyilqndy/nodevenv/api.jrbmc.online/16/bin/node: fork: retry: Resource temporarily unavailablenpm ERR! /home/cyilqndy/nodevenv/api.jrbmc.online/16/bin/node: fork: retry: Resource temporarily unavailablenpm ERR! /home/cyilqndy/nodevenv/api.jrbmc.online/16/bin/node: fork: retry: Resource temporarily unavailablenpm ERR! /home/cyilqndy/nodevenv/api.jrbmc.online/16/bin/node: fork: Resource temporarily unavailablenpm ERR! A complete log of this run can be found in:npm ERR!     /home/cyilqndy/.npm/_logs/2023-11-21T17_30_23_842Z-debug-0.log 
-
-*/
